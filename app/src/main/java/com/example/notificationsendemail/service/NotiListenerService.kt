095bcc4b761df205898b71ee05a7a6959592a979
ParkGiTeak <rl980901@naver.com>
2025-07-08 17:06:32 +0900

    feat: NotiListenerService onNotificationPosted 콜백 호출 시 저장된 패키지 리스트를 확인해서 포함된 패키지의 알림이면 GmailSender Class의 sendMail로 이메일 전송하도록 처리

--- app/src/main/java/com/example/notificationsendemail/service/NotiListenerService.kt
+++ app/src/main/java/com/example/notificationsendemail/service/NotiListenerService.kt
@@ -2,9 +2,51 @@ package com.example.notificationsendemail.service
 
 import android.service.notification.NotificationListenerService
 import android.service.notification.StatusBarNotification
+import android.util.Log
+import com.example.notificationsendemail.BuildConfig
+import com.example.notificationsendemail.datastore.appPrefDataStore
+import com.example.notificationsendemail.datastore.getSavedEmail
+import com.example.notificationsendemail.datastore.notiPacakgeDataStore
+import com.example.notificationsendemail.util.GMailSender
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
 
-class NotiListenerService: NotificationListenerService() {
+class NotiListenerService : NotificationListenerService() {
+    private val serviceScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
+    private var gMailSender: GMailSender? = null
+
     override fun onNotificationPosted(sbn: StatusBarNotification?) {
         super.onNotificationPosted(sbn)
+        if (sbn != null && sbn.packageName != this.packageName) {
+            serviceScope.launch {
+                notiPacakgeDataStore.getSavedPackageList().collect { savedPackageList ->
+                    if (savedPackageList.contains(sbn.packageName)) {
+                        val title = sbn.notification.extras.getString(Notification.EXTRA_TITLE)
+                        val text = sbn.notification.extras.getString(Notification.EXTRA_TEXT)
+                        if (!title.isNullOrEmpty() && !text.isNullOrEmpty()) {
+                            Log.d("PGT", "onNotificationPosted: title: $title / text: $text")
+                            val gmailId = BuildConfig.GmailId
+                            val googleAppPw = BuildConfig.GoogleAppPw
+                            if (gMailSender == null) gMailSender =
+                                GMailSender(userName = gmailId, password = googleAppPw)
+                            appPrefDataStore.getSavedEmail().collect { savedEmail ->
+                                gMailSender?.sendMail(savedEmail, title, text)
+                            }
+                        }
+                    } else {
+                        Log.e("PGT", "onNotificationPosted: 등록되지 않은 Package의 알림")
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        serviceScope.cancel()
+    }
     }
 }
